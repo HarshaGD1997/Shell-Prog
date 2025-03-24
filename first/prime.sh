e6550e9940945f4da627f5553776fd8fdce88c81
HarshaGD1997 <harshagd695@gmail.com>
2025-03-24 16:17:17 +0530

    prime number

---
+++ first/prime.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Enter M : "
+read m
+echo "Enter N : "
+read n
+
+
+for ((i=m; i<=n; i++)); do
+	if((i==1)); then
+		echo "1 is not prime nor consonent"
+		i=2;			
+	fi
+	for((j=2; j<i; j++)); do
+
+		
+		if((i%j == 0)); then
+			flag_val=1
+			break
+		else
+			flag_val=0
+		fi
+	done
+	if((flag_val == 1)); then
+		echo "$i is not prime"
+	else
+		echo "$i is prime"
+	fi
+
+done
+
